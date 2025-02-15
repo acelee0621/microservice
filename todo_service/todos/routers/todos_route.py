230e179c67a476b9a2b9f2327f1745230bf1e923
acelee0621 <acelee0621@outlook.com>
2025-02-16 01:26:46 +0800

    Update todos_route.py

--- todo_service/todos/routers/todos_route.py
+++ todo_service/todos/routers/todos_route.py
@@ -39,7 +39,7 @@ async def get_all_todos(
         str | None, Query(description="Search todos by title or description")
     ] = None,
     order_by: Annotated[
-        str | None, Query(description="Order by field (e.g., created_at desc)")
+        str | None, Query(description="Order by field (e.g., created_at desc/asc)")
     ] = None,
     service: TodosService = Depends(get_todos_service),
     current_user: UserRead = Depends(get_current_user),
